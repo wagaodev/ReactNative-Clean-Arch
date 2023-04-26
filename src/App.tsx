feecc36a0f30758b307ae73f02a5cb2f35e856a5
Wagão <wcfx.dev@gmail.com>
2023-04-26 05:00:11 -0300

    feat: adding theme provider to appjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {RootModuleNavigator} from './module/navigation';
-import {NavigationContainer} from '@react-navigation/native';
-// import {ThemeProvider} from 'styled-components/native';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RootModuleNavigator } from './module/navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components/native';
+
+import { theme } from './global';
 
 export const App = () => {
   return (
-    // <ThemeProvider theme={theme}>
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <RootModuleNavigator />
-        <StatusBar
-          barStyle="light-content"
-          translucent
-          backgroundColor="transparent"
-        />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <RootModuleNavigator />
+          <StatusBar
+            barStyle='light-content'
+            translucent
+            backgroundColor='transparent'
+          />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 };
 
