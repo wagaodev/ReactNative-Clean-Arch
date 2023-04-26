0e1212170db8d17d04f57d7589745de56e418271
Wagão <wcfx.dev@gmail.com>
2023-04-26 05:00:47 -0300

    feat: navigators helper added

--- src/module/views/starter/index.tsx
+++ src/module/views/starter/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
 export const Starter = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{alignSelf: 'center'}}>Hello Starter</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ alignSelf: 'center' }}>Hello Starter</Text>
     </View>
   );
 };
