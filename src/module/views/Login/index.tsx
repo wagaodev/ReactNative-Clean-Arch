bef85dabb7cc1613d391225cf36a76669c4029ce
Wagão <wcfx.dev@gmail.com>
2023-05-05 01:08:05 -0300

    feat: login can handle with pass and email fields

--- src/module/views/Login/index.tsx
+++ src/module/views/Login/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as S from './styles';
+import { Alert } from 'react-native';
 
 export const Login = () => {
-  return <S.Login />;
+  const [password, setPassword] = useState('');
+  return (
+    <S.Login
+      value={password}
+      onChange={(password) => setPassword(password)}
+      onSubmit={() => Alert.alert('Clicou')}
+    />
+  );
 };
