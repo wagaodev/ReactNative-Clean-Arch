1e6ea320f44048a24f98bdd68ac625ac37023866
wagaodev <wcfx.dev@gmail.com>
2023-05-07 16:17:08 -0300

    feat: error handler component created

---
+++ src/module/components/HandleError/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const AlertContainer = styled.View`
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+  border-radius: 4px;
+  border-width: 1px;
+  padding: 8px 16px;
+`;
+
+export const AlertText = styled.Text`
+  color: #721c24;
+  font-size: 16px;
+`;
